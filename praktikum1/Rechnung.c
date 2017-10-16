32e6356fbefefcb957616637f398d6c0fb8d13a1
Maja Rother <majarother@Majas-MacBook-Pro.local>
2017-10-16 22:42:32 +0200

    adding praktikum 1 rechnung file

---
+++ praktikum1/Rechnung.c
@@ -0,0 +1,25 @@
+/****************************************************************
+   Aufgabe
+Autor
+Datum
+1.2
+S. Rohjans
+10.03.2016
+Kurzbeschreibung: Begrüßung! *****************************************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+printf("Herzlich willkommen, ich heisse Austin!!\n");
+printf("Und tsch%c%c!\n", 129, 225);
+return 0; 
+
+}
+
+/*
+ * 1) write and save your program program.c
+ * 2) go to terminal
+ * 3) gcc program.c -o program.out
+ * 4) terminal: ./program.out
+ */
