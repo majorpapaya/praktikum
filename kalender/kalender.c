98ba9004a67794dd1d90aff87f02da7ad9c47b6b
majorpapaya <maja.rother@web.de>
2018-01-10 15:35:27 +0100

    Version 2

--- kalender/kalender.c
+++ kalender/kalender.c
@@ -3,7 +3,6 @@ Datum:
 Autor:
 Version:
 Kurzbeschreibung:
-
 ************************************/
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -14,25 +13,31 @@ Kurzbeschreibung:
 
 int ist_schaltjahr(int jahr);
 int anzahl_tage_im_monat(int monat, int jahr);
-int kalenderJahr(jahr, erster);
+//int kalenderJahr(jahr, erster);
 
 int main(void) {
 
 	int jahr = 0;
 	int erster = 0;
-	double mat[5][5];
+	int anzahl_tage = 0;
 
 
-	printf("Geben Sie ein Jahr ein : \n");
+	printf("Geben Sie ein Jahr ein : \n\n");
 	scanf("%d", &jahr);
 
-	printf("Welcher Tag ist der 1. Januar?\n ( 1 = Montag, 2 = Dienstag usw.) \n");
+	printf("\nWelcher Tag ist der 1. Januar?\n( 1 = Montag, 2 = Dienstag usw.) \n\n");
 	scanf("%d", &erster);
 
-	printf("Kalender\n\n");
-	kalenderJahr(jahr, erster);
+	printf("\nKalender\n");
+	for (int i = 1; i <= 12; i++) {
+		printf("\nMonat %d\n\n", i);
+		
+	anzahl_tage = anzahl_tage_im_monat(i, jahr);
+	printf("Anzahl der Tag sind: %d\n\n", anzahl_tage);
 
-
+	erster = KalenderMonat(anzahl_tage, erster);
+	
+	}
 	return 0;
 }
 
@@ -49,35 +54,40 @@ int anzahl_tage_im_monat(int monat, int jahr) {
 		break;
 	default: tage = 31;
 	}
+	return tage;
 }
 
 int ist_schaltjahr(int jahr) {
 	return ((jahr % 4 == 0) && (jahr % 100 != 0)) || (jahr % 400 == 0);
 }
 
-int KalenderMonat(int anzahlTage, int erster, int tag) {
+int KalenderMonat(int anzahlTage, int erster) {
 
+	long tag;
 	//Spaltenbeschriftung
-	printf(" Mo  Di  Mi  Do  Fr  Sa  So\n");
+	printf("  Mo  Di  Mi  Do  Fr  Sa  So\n");
 	printf("---------------------------\n");
 
 	//Einrücken des ersten Kalendertags
-	printf("%*s", (int)(4 * erster), "");
-
+	for (int i = 1; i < erster; i++) { //printf("%*s", 4 * erster, "");
+		printf("    ");
+	}
 	//Ausgabe des Kalenders
 	for (tag = 1; tag <= anzahlTage; tag = tag + 1) {
-		printf("%4d", tag);
-		if ((tag + erster) % 7 == 0)
+		printf("%4i", tag);
+		if (erster > 6) {
+			erster = 0;
 			printf("\n");
+		}
+		erster++;
 	}
 	printf("\n");
-
-	return (tag + erster = 1) % 7;
+	return erster;
 }
 
-int kalenderJahr(jahr, erster) {
+/*int kalenderJahr(jahr, erster) {
 	for (int i = 1; i <= 12; i++) {
 		printf("Monat %d", i)
 			KalenderMonat()
 	}
-}
+}*/
