8f7eeddee6766850823b53907ed1273f4e4ca1fa
majorpapaya <maja.rother@web.de>
2018-01-24 16:40:17 +0100

    Teiler - SS16 hochgeladen

---
+++ KLAUSUR/teiler.c
@@ -0,0 +1,22 @@
+/****************************************************************
+Aufgabe 1.2
+Autor Maja Rother
+Datum 12.12.2017
+Kurzbeschreibung:
+*****************************************************************/
+
+// Präprozessordirektiven
+#define _CRT_SECURE_NO_WARNINGS
+
+// Header
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+int main(void)
+{
+
+
+
+	return 0;
+}
